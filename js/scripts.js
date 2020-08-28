f329d2d66e729b8a2c76ac3b2912f2a4c9960810
Tietohallinto <tietohallinto@BC-minimac053.local>
2020-08-28 11:24:25 +0300

    Function task

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,33 @@
 function calculate(order) {
-    var result;
-    var firstInput = parseInt(document.getElementById('firstinput').value);
-    var secondInput = parseInt(document.getElementById('secondinput').value);
+  var result;
+  var firstInput = parseInt(document.getElementById("firstinput").value);
+  var secondInput = parseInt(document.getElementById("secondinput").value);
 
-    if (order === 'add') {
-        result = add(firstInput, secondInput);
-    } else if (order === 'subtract') {
-        result = subtract(firstInput, secondInput);
-    }
+  if (order === "add") {
+    result = add(firstInput, secondInput);
+  } else if (order === "subtract") {
+    result = subtract(firstInput, secondInput);
+  } else if (order === "multiply") {
+    result = multiply(firstInput, secondInput);
+  } else if (order === "divide") {
+    result = divide(firstInput, secondInput);
+  }
 
-    document.getElementById('final-result').innerText = result;
+  document.getElementById("final-result").innerText = result;
 }
 
-function add(a, b){
-    return a + b;
+function add(a, b) {
+  return a + b;
 }
 
 function subtract(a, b) {
-
+  return a - b;
 }
 
 function multiply(a, b) {
-
+  return a * b;
 }
 
 function divide(a, b) {
-
+  return a / b;
 }
